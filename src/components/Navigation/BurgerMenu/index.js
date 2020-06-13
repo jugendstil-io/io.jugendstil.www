cfc54b1b60a524d45f81c62ca6687c084ef3b2e6
Mathias Schilling <m@matchilling.com>
2020-06-13 12:31:20 +0200

    Active hamburger menu on small screens

--- src/components/Navigation/BurgerMenu/index.js
+++ src/components/Navigation/BurgerMenu/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "./index.scss"
+import URL from "../../../url"
 
 export default class BurgerMenu extends React.Component {
   static componentId = "burger-menu"
@@ -23,18 +24,49 @@ export default class BurgerMenu extends React.Component {
   }
 
   render() {
+    const { isOpen } = this.state
     return (
-      <div className={BurgerMenu.componentId}>
-        <div
-          className={`${BurgerMenu.componentId}-icon ${
-            this.state.isOpen ? "open" : "closed"
-          }`}
-          onClick={this.toggle}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
+      <div
+        className={`${BurgerMenu.componentId} ${
+          this.state.isOpen ? "open" : "closed"
+        }`}
+      >
+        {!isOpen && (
+          <div
+            className={`${BurgerMenu.componentId}-icon`}
+            onClick={this.toggle}
+          >
+            <span />
+            <span />
+            <span />
+          </div>
+        )}
+
+        {isOpen && (
+          <React.Fragment>
+            <div
+              className={`${BurgerMenu.componentId}-icon`}
+              onClick={this.toggle}
+            >
+              <span />
+              <span />
+            </div>
+            <div>
+              <a onClick={this.toggle} href={URL.SERVICE.INDEX}>
+                Services
+              </a>
+              <a onClick={this.toggle} href={URL.ABOUT_US}>
+                Abouts Us
+              </a>
+              <a onClick={this.toggle} href={URL.THOUGHTS_ON_PROGRAMMING}>
+                Thoughts
+              </a>
+              <a onClick={this.toggle} href={URL.CONTACT}>
+                Contact
+              </a>
+            </div>
+          </React.Fragment>
+        )}
       </div>
     )
   }
