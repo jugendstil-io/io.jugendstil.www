93d86fe461d67e45a369f4d1dd751de1b5cbc6a3
Mathias Schilling <m@matchilling.com>
2020-06-13 13:32:31 +0200

    Prevent scrolling when burger menu is open

--- src/components/Navigation/BurgerMenu/index.js
+++ src/components/Navigation/BurgerMenu/index.js
@@ -1,6 +1,8 @@
 import React from "react"
-import "./index.scss"
+import Helmet from "react-helmet"
 import URL from "../../../url"
+
+import "./index.scss"
 
 export default class BurgerMenu extends React.Component {
   static componentId = "burger-menu"
@@ -22,48 +24,51 @@ export default class BurgerMenu extends React.Component {
   render() {
     const { isOpen } = this.state
     return (
-      <div
-        className={`${BurgerMenu.componentId} ${
-          this.state.isOpen ? "open" : "closed"
-        }`}
-      >
-        {!isOpen && (
-          <div
-            className={`${BurgerMenu.componentId}-icon`}
-            onClick={this.toggle}
-          >
-            <span />
-            <span />
-            <span />
-          </div>
-        )}
-
-        {isOpen && (
-          <React.Fragment>
+      <React.Fragment>
+        <Helmet bodyAttributes={{ class: isOpen ? "overflow-hidden" : "" }} />
+        <div
+          className={`${BurgerMenu.componentId} ${
+            this.state.isOpen ? "open" : "closed"
+          }`}
+        >
+          {!isOpen && (
             <div
               className={`${BurgerMenu.componentId}-icon`}
               onClick={this.toggle}
             >
               <span />
               <span />
+              <span />
             </div>
-            <div>
-              <a onClick={this.toggle} href={URL.SERVICE.INDEX}>
-                Services
-              </a>
-              <a onClick={this.toggle} href={URL.ABOUT_US}>
-                Abouts Us
-              </a>
-              <a onClick={this.toggle} href={URL.THOUGHTS_ON_PROGRAMMING}>
-                Thoughts
-              </a>
-              <a onClick={this.toggle} href={URL.CONTACT}>
-                Contact
-              </a>
-            </div>
-          </React.Fragment>
-        )}
-      </div>
+          )}
+
+          {isOpen && (
+            <React.Fragment>
+              <div
+                className={`${BurgerMenu.componentId}-icon`}
+                onClick={this.toggle}
+              >
+                <span />
+                <span />
+              </div>
+              <div>
+                <a onClick={this.toggle} href={URL.SERVICE.INDEX}>
+                  Services
+                </a>
+                <a onClick={this.toggle} href={URL.ABOUT_US}>
+                  Abouts Us
+                </a>
+                <a onClick={this.toggle} href={URL.THOUGHTS_ON_PROGRAMMING}>
+                  Thoughts
+                </a>
+                <a onClick={this.toggle} href={URL.CONTACT}>
+                  Contact
+                </a>
+              </div>
+            </React.Fragment>
+          )}
+        </div>
+      </React.Fragment>
     )
   }
 }
