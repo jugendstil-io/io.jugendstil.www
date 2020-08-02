6dc1bfdc37192541559f482b23062cba5191f8fc
Mathias Schilling <m@matchilling.com>
2020-08-02 12:19:15 +0200

    Open external links in new tab

--- src/components/Button/Primary/index.js
+++ src/components/Button/Primary/index.js
@@ -2,12 +2,60 @@ import PropTypes from "prop-types"
 import React from "react"
 
 import "./index.scss"
+import { graphql, useStaticQuery } from "gatsby"
 
-const Button = ({ url, label, style }) => (
-  <a className="btn btn-primary" href={url} style={style}>
-    <span className="label">{label}</span>
-  </a>
-)
+const isValidUrl = string => {
+  const pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+    "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+    "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+    "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+    "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ) // fragment locator
+
+  return !!pattern.test(string)
+}
+
+const Button = ({ url, label, style }) => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            siteUrl
+          }
+        }
+      }
+    `
+  )
+
+  const className = "btn btn-primary"
+  const isExternal =
+    isValidUrl(url) &&
+    new URL(url).host !== new URL(site.siteMetadata.siteUrl).host
+
+  if (isExternal) {
+    return (
+      <a
+        className={className}
+        href={url}
+        rel="noreferrer"
+        style={style}
+        target="_blank"
+      >
+        <span className="label">{label}</span>
+      </a>
+    )
+  }
+
+  return (
+    <a className={className} href={url} style={style}>
+      <span className="label">{label}</span>
+    </a>
+  )
+}
 
 Button.propTypes = {
   label: PropTypes.string,
